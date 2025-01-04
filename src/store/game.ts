5baa9995c170724501e1c3ceea15e30331f69c10
barbarahuem <huem.barbara@gmail.com>
2025-01-04 18:56:37 +0100

    add game store

---
+++ src/store/game.ts
@@ -0,0 +1,43 @@
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
+
+// Enums for GameMode and GameActions
+export enum GameMode {
+  TIMER = 'timer',
+  MAX_FLIPS = 'maxFlips',
+}
+
+export enum GameActions {
+  UPDATE_LEVEL = 'UPDATE_LEVEL',
+  UPDATE_NICKNAME = 'UPDATE_NICKNAME',
+  UPDATE_GAME_MODE = 'UPDATE_GAME_MODE',
+}
+
+export const useGameStore = defineStore('game', () => {
+  const level = ref(0);
+  const nickname = ref('');
+  const gameMode = ref<GameMode | ''>('');
+
+  const updateLevel = (newLevel: number) => {
+    level.value = newLevel;
+  };
+
+  const updateNickname = (newNickname: string) => {
+    nickname.value = newNickname;
+  };
+
+  const updateGameMode = (newGameMode: GameMode) => {
+    gameMode.value = newGameMode;
+  };
+
+  return {
+    level,
+    nickname,
+    gameMode,
+    updateLevel,
+    updateNickname,
+    updateGameMode,
+    GameMode,
+    GameActions,
+  };
+});
