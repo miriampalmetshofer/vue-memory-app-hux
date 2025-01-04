a1317b25ba07a2a5bcb90b6d7e1d3d64d1458334
barbarahuem <huem.barbara@gmail.com>
2025-01-04 18:57:33 +0100

    add simple input to test store

--- src/components/screens/GameScreen.vue
+++ src/components/screens/GameScreen.vue
@@ -1,8 +1,16 @@
-<script></script>
+<script setup lang="ts">
+import { useGameStore } from '../../store/game';
+
+const gameStore = useGameStore();
+
+</script>
 
 <template>
   <div>
     <h1>Game Screen</h1>
+    <div>
+      <p>Player: {{ gameStore.nickname }}</p>
+    </div>
   </div>
 
 </template>
