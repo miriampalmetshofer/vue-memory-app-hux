5c6c0c7fe73ea88ec24b4a6ff937a6377ddd43e6
barbarahuem <huem.barbara@gmail.com>
2025-01-08 19:34:49 +0100

    short fix

--- src/utils/getTopThreeScores.ts
+++ src/utils/getTopThreeScores.ts
@@ -1,4 +1,6 @@
-export function getTopThreeScores(games: Game[]) {
+import { GameData } from "../types/GameData";
+
+export function getTopThreeScores(games: GameData[]) {
   if (games) {
     const sortedGames = games.sort((a, b) => b.level - a.level);
     const topThree = sortedGames.slice(0, 3);
